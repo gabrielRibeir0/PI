4fa558bf6bc938d7982bc1beed5ef211ff5ef9b3
Gabriel Ribeiro <a104171@alunos.uminho.pt>
2023-03-08 09:55:50 +0000

    ficha 2 done

--- ficha2/ficha2.c
+++ ficha2/ficha2.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 //Ex 1
 float multInt1(int n, float m){
     float r = 0;
@@ -32,4 +34,65 @@ int mdc(int a, int b){
         if (a % i == 0 && b % i == 0)
             return i;
     }
+}
+
+//Ex 4
+int mdc2(int a, int b){
+    int i = 0;
+    while(a > 0 && b > 0){
+        if (a>b){
+            a -= b;
+        }
+        else{
+            b -= a;
+        }
+        i++;
+    }
+    printf("%d\n", i);       
+    return a+b;   
+}
+
+//Ex 5
+int mdc3(int a, int b){
+    int i = 0;
+    while(a > 0 && b > 0){
+        if (a>b){
+            a %= b;
+        }
+        else{
+            b %= a;
+        }
+        i++;
+    }
+    printf("%d\n", i);       
+    return a+b;   
+}
+
+//Ex 6
+//a)
+int fib1(int n){
+    if(n < 2){
+        return n;
+    }
+    else{
+        return (fib1(n-1) + fib1(n-2));
+    }
+}
+
+//b)
+int fib2(int n){
+    int a = 0, b = 1, i = 2,r;
+    if (n < 2)
+        return n;
+    while(i <=  n){ 
+        r = a + b;
+        a = b;
+        b = r;
+        i++;
+    }
+    return r;
+}
+
+int main(){
+    return 0;
 }
